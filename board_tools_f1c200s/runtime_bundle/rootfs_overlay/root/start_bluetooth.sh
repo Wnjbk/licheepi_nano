b5071ca86a26c23a0039d2f74adc73644ade28a6
wnk64 <2630170240@qq.com>
2026-08-07 06:17:08 -0700

    restore-rtl8723bu-bluetooth-coexistence

--- board_tools_f1c200s/runtime_bundle/rootfs_overlay/root/start_bluetooth.sh
+++ board_tools_f1c200s/runtime_bundle/rootfs_overlay/root/start_bluetooth.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+PATH=/usr/sbin:/usr/bin:/sbin:/bin
+
 export DBUS_SYSTEM_BUS_ADDRESS=${DBUS_SYSTEM_BUS_ADDRESS:-unix:path=/var/run/dbus/system_bus_socket}
 MODULE_DIR=${MODULE_DIR:-/root/roms/system_modules/rtl8723bluetooth}
 

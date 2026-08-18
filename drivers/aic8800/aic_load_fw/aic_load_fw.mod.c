533a2d8b8972c2598705772c603ff9b1b590301e
wnk <wnk@localhost>
2026-08-18 08:38:54 -0700

    baseline: complete UGREEN AIC8800D80 loader and fdrv source

---
+++ drivers/aic8800/aic_load_fw/aic_load_fw.mod.c
@@ -0,0 +1,38 @@
+#include <linux/module.h>
+#include <linux/build-salt.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(.gnu.linkonce.this_module) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+MODULE_INFO(depends, "");
+
+MODULE_ALIAS("usb:vA69Cp8800d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:vA69Cp8801d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:vA69Cp8D80d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:vA69Cp8D81d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:vA69Cp8D40d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:vA69Cp8D41d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v368Bp8D90d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v368Bp8D91d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v368Bp8D99d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v368Bp8D92d*dc*dsc*dp*ic*isc*ip*in*");
+
+MODULE_INFO(srcversion, "0AC838E8FB814FC32A3D660");

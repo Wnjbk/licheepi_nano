c6da0354409fea1ae583cdeb46967f9152643066
wnk <wnk@localhost>
2026-09-05 05:20:47 -0700

    Disable LVGL perf monitor in idle OSD candidate

--- lv_conf.h
+++ lv_conf.h
@@ -4,7 +4,7 @@
 #define LV_COLOR_DEPTH 16
 #define LV_MEM_SIZE (96U * 1024U)
 #define LV_USE_LOG 0
-#define LV_USE_PERF_MONITOR 1
+#define LV_USE_PERF_MONITOR 0
 #define LV_FONT_MONTSERRAT_14 1
 
 #endif
